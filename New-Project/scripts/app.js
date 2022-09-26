1c59c5c0e8ee8e62fe3fb480e29d7ddd6e8a993d
LukaSuliauri <109594413+LukaSuliauri@users.noreply.github.com>
2022-09-26 16:01:22 +0400

    Add files via upload

--- New-Project/scripts/app.js
+++ New-Project/scripts/app.js
@@ -6,6 +6,17 @@ const getData = async(url) => {
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
+    
     const productsData = await getData(`${baseUrl}/products`)
     _render(_getById('product-row') , productsData.products.map( product => $product(product)).join('') )
+
+
+    _getById('search-btn').addEventListener('click', async () =>{
+        const search = _getById('search-box').value.trim()
+        const sortedProducts = await getData(`${baseUrl}/products/search?q=${search}`)
+        _render(_getById('product-row') , sortedProducts.products.map( product => $product(product)).join('') )
+    })
+
+    
 })
+
--- New-Project/scripts/components.js
+++ New-Project/scripts/components.js
@@ -1,6 +1,6 @@
 const $product = (_) => { 
     const {id, thumbnail, title, brand, description, rating, category, price, discountPercentage} = _
-    return `<div class="col-md-4 my-5">
+    return `<div class="col-lg-4 col-md-6 col-sm-12 my-5">
                 <div class="product-card" id="${id}">
                      <div class="card" style="width: 18rem;">
                         <img src="${thumbnail}" class="card-img-top" alt="...">
@@ -14,7 +14,8 @@ const $product = (_) => {
                                     <p class="product-category">Category:${category}</p>
                                     <p class="product-price">Price:${price}</p>
                                     <p class="product-discount">Discount Percentage: ${discountPercentage}</p>  
-                                    <a href="#" class="btn btn-primary">Buy Now</a>
+                                    <a href="#" class="btn btn-success">Buy Now</a>
+                                    <a href="#" class="btn btn-primary d-flex float-end">View More</a>
                                 </div>
                             </div>
                     </div>
